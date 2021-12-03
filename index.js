428db1084ee84e0d2db1880094eb88861a72524e
mldgs <76591534+mldgs@users.noreply.github.com>
2021-12-03 22:40:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+"
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 if(randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML ="Draw!";
+  document.querySelector(".header-text").innerHTML ="Draw!";
 }
 else if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerText ="\u{1F600}Player 1 Wins!";
+  document.querySelector(".header-text").innerText ="\u{1F600}Player 1 Wins!";
 }
 else {
-  document.querySelector("h1").innerHTML ="Player 2 Wins!\u{1F600}";
+  document.querySelector(".header-text").innerHTML ="Player 2 Wins!\u{1F600}";
 }
